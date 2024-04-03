7838204c232900a4f1e0a77d39313afcf60ed5f8
Manju Ashish Buddhaghosh <43272580+thebuddhaghosh@users.noreply.github.com>
2024-04-03 18:36:47 +0530

    Portfolio Template

---
+++ js/bgs_sliders.js
@@ -0,0 +1,16 @@
+$(".testimonial-carousel").owlCarousel({
+  loop: true,
+  margin: 15,
+  responsiveClass: true,
+  loop: true,
+  dots: false,
+  items: 2.5,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 2500,
+  autoplayHoverPause: true,
+  navText: [
+    "<i class='fa-solid fa-angle-left'></i>",
+    "<i class='fa-solid fa-angle-right'></i>",
+  ],
+});
